11f3254ea466d00f715ec18aa3e9cd949a6a0087
bisl3y <80480751+bisl3y@users.noreply.github.com>
2023-03-01 12:02:39 +0000

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const grassImg = new tileBlock('ground',1);
-grassImg.src = "img/grass_iso_0.gif";
+grassImg.src = "img/grassBlock_iso_0.gif";
 const crateImg = new tileBlock('prop',1);
 crateImg.src = "img/crate_iso_0.gif";
 const dockImg = new tileBlock('prop',4);
@@ -16,6 +16,7 @@ let block = crateImg;
 let angle = 0;
 let mapWidth = 16;
 let mapHeight = 16;
+let outline = false;
 
 function drawIso() {
     renderStyle = "Iso";
@@ -40,7 +41,12 @@ canvas.addEventListener("click", setTile);
 
 function setTile(event) {
     if (renderStyle == "Iso") {
+		if(outline){
+		map.moveGroundUp(event.offsetX, event.offsetY, event.altKey);	
+		}
+		else{
         map.setTileIso(event.offsetX, event.offsetY);
+		}
         drawIso();
     } else {
         map.setTile2d(event.offsetX, event.offsetY);
@@ -60,6 +66,14 @@ function zoom(z) {
     }
     drawMap();
 };
+
+addEventListener("keydown", toggleGrid);
+
+function toggleGrid(event){
+	if (event.key == 'o'){
+		outline = !outline;
+	}
+}
 
 addEventListener("keydown", rotateMap);
 
@@ -125,13 +139,26 @@ if(event.buttons == 4){
 }
 }
 
+canvas.addEventListener("mousemove", highlight);
+
+function highlight(event){
+	let hlTile = map.findTileID(event.offsetX, event.offsetY);
+	let col = hlTile[0]
+	let row = hlTile[1]
+	map.clearHighlights();
+	if (col >= 0 && col < map.mapCols && row >= 0 && row < map.mapRows){
+	map.grid[col][row].highlight = true;
+	drawMap();
+	}
+}
+
 function newMap(){
 	map = new imageMap(mapWidth, mapHeight);
 	drawMap();
 }
 angle = 0
 newMap();
-map.grid[3][3].upperImage = crateImg;
+
 crateImg.onload = () => {
     drawMap();
 }
--- js/iso.js
+++ js/iso.js
@@ -1,27 +1,35 @@
 class tileBlock extends Image {
-    constructor(type,angleNum) {
+    constructor(type, angleNum) {
         super();
-		this.blockType = type;
-		this.angles = angleNum;
-	}
+        this.blockType = type;
+        this.angles = angleNum;
+    }
 }
 
-const defaultIso = new tileBlock('ground',1);
-defaultIso.src = "img/paving_iso_0.gif";
+const defaultIso = new tileBlock('ground', 1);
+defaultIso.src = "img/pavingBlock_iso_0.gif";
 
 class tile {
     constructor() {
         this.shade = 'white'
-            this.fillTile = function fillTile() {
+            this.vOffset = 1;
+        this.fillTile = function fillTile() {
             this.shade = color;
+			this.highlight = false;
+
         }
-        this.drawIso = function drawIso(x, y, tileSize) {
-            ctx.beginPath();
+		this.tilePath = function tilePath(x, y, tileSize) {
+		ctx.beginPath();
             ctx.moveTo(x, y);
             ctx.lineTo(x + tileSize, y - tileSize / 2);
             ctx.lineTo(x, y - tileSize);
             ctx.lineTo(x - tileSize, y - tileSize / 2);
             ctx.closePath();
+        }
+		
+		
+        this.drawIso = function drawIso(x, y, tileSize) {
+            this.tilePath(x, y, tileSize);
             ctx.fillStyle = this.shade;
             ctx.fill();
             ctx.stroke();
@@ -33,38 +41,53 @@ class tile {
             ctx.fillRect(x, y, tileSize, tileSize);
             ctx.stroke();
         }
+		    this.drawOutline = function drawOutline(x, y, tileSize) {
+            this.tilePath(x, y, tileSize);
+            ctx.stroke();
+        }
+		    this.drawOverlay = function drawOverlay(x, y, tileSize) {
+            this.tilePath(x, y, tileSize);
+			ctx.fillStyle = 'rgba(0,255,255,0.5)';
+            ctx.fill();
+        }
     }
 };
 
 class imageTile extends tile {
     constructor() {
         super();
-        this.ground = defaultIso;		
-		this.prop = null;
+        this.ground = defaultIso;
+        this.prop = null;
         this.fillTile = function fillTile() {
-            if(block.blockType == 'prop'){
-			this.prop = block;	
-			}
-			else{
-			this.ground = block;}
+            if (block.blockType == 'prop') {
+                this.prop = block;
+            } else {
+                this.ground = block;
+            }
         }
         this.drawIso = function drawIso(x, y, tileSize) {
-			let widthOffsetGround = this.ground.width/this.ground.angles
-            ctx.drawImage(this.ground, 0,0, widthOffsetGround, this.ground.height, x - tileSize, y - tileSize * this.ground.height / 64, widthOffsetGround * tileSize / 64, this.ground.height * tileSize / 64);
-			if(this.prop instanceof tileBlock){
-				let widthOffsetProp = this.prop.width/this.prop.angles
-				let angleOffset = angle/360;
-				if(this.prop.angles == 4){
-				ctx.drawImage(this.prop, this.prop.width*angleOffset, 0, widthOffsetProp, this.prop.height, x - tileSize, y - tileSize * this.prop.height / 64, widthOffsetProp * tileSize / 64, this.prop.height * tileSize / 64);}
-				else{
-				ctx.drawImage(this.prop, 0, 0, widthOffsetProp, this.prop.height, x - tileSize, y - tileSize * this.prop.height / 64, widthOffsetProp * tileSize / 64, this.prop.height * tileSize / 64);	
-				}
+            let widthOffsetGround = this.ground.width / this.ground.angles;
+            ctx.drawImage(this.ground, 0, 0, widthOffsetGround, this.ground.height, x - tileSize, y - tileSize * this.ground.height / 64 + tileSize * this.vOffset, widthOffsetGround * tileSize / 64, this.ground.height * tileSize / 64);
+            if (outline == true) {
+			this.drawOutline(x, y + (this.vOffset - 1) * tileSize, tileSize);		
+            }
+			if (this.highlight == true) {
+			this.drawOverlay(x, y + (this.vOffset - 1) * tileSize, tileSize);
 			}
+            if (this.prop instanceof tileBlock) {
+                let widthOffsetProp = this.prop.width / this.prop.angles
+                    let angleOffset = angle / 360;
+                if (this.prop.angles == 4) {
+                    ctx.drawImage(this.prop, this.prop.width * angleOffset, 0, widthOffsetProp, this.prop.height, x - tileSize, y - tileSize * this.prop.height / 64 +(this.vOffset-1)*tileSize, widthOffsetProp * tileSize / 64, this.prop.height * tileSize / 64);
+                } else {
+                    ctx.drawImage(this.prop, 0, 0, widthOffsetProp, this.prop.height, x - tileSize, y - tileSize * this.prop.height / 64 +(this.vOffset-1)*tileSize, widthOffsetProp * tileSize / 64, this.prop.height * tileSize / 64);
+                }
+            }			
         }
     }
 }
 
-class map {	
+class map {
     constructor(mapCols, mapRows) {
         this.mapCols = mapCols;
         this.mapRows = mapRows;
@@ -101,45 +124,45 @@ class map {
         this.drawIso = function drawIso(x, y, tileSize) {
             let rotOffsetX = (this.mapCols / 2 * tileSize) - (this.mapRows / 2 * tileSize);
             let rotOffsetY = (this.mapRows / 2 * tileSize / 2) + (this.mapCols / 2 * tileSize / 2);
-			rotOffsetX = 0
-			rotOffsetY = 0
-            if (angle == 0) {
-				rotOffsetY = -this.mapRows/2*tileSize/2 -this.mapCols/2*tileSize/2
-				rotOffsetX = this.mapRows/2*tileSize -this.mapCols/2*tileSize
-                for (let col = 0; col < this.mapCols; col++) {
-                    for (let row = 0; row < this.mapRows; row++) {
-						let rowOffset = row * tileSize;
-                        this.grid[col][row].drawIso(col * tileSize - rowOffset + x + rotOffsetX, col * tileSize / 2 + rowOffset / 2 + y + rotOffsetY, tileSize);
-                    }
+            rotOffsetX = 0
+                rotOffsetY = 0
+                if (angle == 0) {
+                    rotOffsetY = -this.mapRows / 2 * tileSize / 2 - this.mapCols / 2 * tileSize / 2 
+                        rotOffsetX = this.mapRows / 2 * tileSize - this.mapCols / 2 * tileSize
+                        for (let col = 0; col < this.mapCols; col++) {
+                            for (let row = 0; row < this.mapRows; row++) {								
+                                let rowOffset = row * tileSize;
+                                this.grid[col][row].drawIso(col * tileSize - rowOffset + x + rotOffsetX, col * tileSize / 2 + rowOffset / 2 + y + rotOffsetY, tileSize);
+                            }
+                        }
+                } else if (angle == 90) {
+                    rotOffsetY = this.mapRows / 2 * tileSize / 2 - this.mapCols / 2 * tileSize / 2 - tileSize / 2
+                        rotOffsetX = this.mapRows / 2 * tileSize + this.mapCols / 2 * tileSize - tileSize
+                        for (let col = 0; col < this.mapCols; col++) {
+                            for (let row = this.mapRows - 1; row >= 0; row--) {
+                                let rowOffset = row * tileSize;
+                                this.grid[col][row].drawIso(-col * tileSize - rowOffset + x + rotOffsetX, col * tileSize / 2 - rowOffset / 2 + y + rotOffsetY, tileSize);
+                            }
+                        }
+                } else if (angle == 180) {
+                    rotOffsetY = this.mapRows / 2 * tileSize / 2 + this.mapCols / 2 * tileSize / 2 - tileSize
+                        rotOffsetX = -this.mapRows / 2 * tileSize + this.mapCols / 2 * tileSize
+                        for (let col = this.mapCols - 1; col >= 0; col--) {
+                            for (let row = this.mapRows - 1; row >= 0; row--) {
+                                let rowOffset = row * tileSize;
+                                this.grid[col][row].drawIso(-col * tileSize + rowOffset + x + rotOffsetX, -col * tileSize / 2 - rowOffset / 2 + y + rotOffsetY, tileSize);
+                            }
+                        }
+                } else if (angle == 270) {
+                    rotOffsetY = -this.mapRows / 2 * tileSize / 2 + this.mapCols / 2 * tileSize / 2 - tileSize / 2
+                        rotOffsetX = -this.mapRows / 2 * tileSize - this.mapCols / 2 * tileSize + tileSize
+                        for (let col = this.mapCols - 1; col >= 0; col--) {
+                            for (let row = 0; row < this.mapRows; row++) {
+                                let rowOffset = row * tileSize;
+                                this.grid[col][row].drawIso(col * tileSize + rowOffset + x + rotOffsetX, -col * tileSize / 2 + rowOffset / 2 + y + rotOffsetY, tileSize);
+                            }
+                        }
                 }
-            } else if (angle == 90) {
-				rotOffsetY = this.mapRows/2*tileSize/2 -this.mapCols/2*tileSize/2 -tileSize/2
-				rotOffsetX = this.mapRows/2*tileSize +this.mapCols/2*tileSize -tileSize
-                for (let col = 0; col < this.mapCols; col++) {
-                    for (let row = this.mapRows - 1; row >= 0; row--) {
-						let rowOffset = row * tileSize;
-                        this.grid[col][row].drawIso(-col * tileSize - rowOffset + x + rotOffsetX, col * tileSize / 2 - rowOffset / 2 + y + rotOffsetY, tileSize);
-                    }
-                }
-            } else if (angle == 180) {
-				rotOffsetY = this.mapRows/2*tileSize/2 + this.mapCols/2*tileSize/2 -tileSize
-				rotOffsetX = -this.mapRows/2*tileSize + this.mapCols/2*tileSize
-                for (let col = this.mapCols - 1; col >= 0; col--) {
-                    for (let row = this.mapRows - 1; row >= 0; row--) {
-						let rowOffset = row * tileSize;
-                        this.grid[col][row].drawIso(-col * tileSize + rowOffset + x + rotOffsetX, -col * tileSize / 2 - rowOffset / 2 + y + rotOffsetY, tileSize);
-                    }
-                }
-            } else if (angle == 270) {
-				rotOffsetY = -this.mapRows/2*tileSize/2 +this.mapCols/2*tileSize/2 -tileSize/2
-				rotOffsetX = -this.mapRows/2*tileSize -this.mapCols/2*tileSize + tileSize
-                for (let col = this.mapCols - 1; col >= 0; col--) {
-                    for (let row = 0; row < this.mapRows; row++) {
-						let rowOffset = row * tileSize;
-                        this.grid[col][row].drawIso(col * tileSize + rowOffset + x + rotOffsetX, -col * tileSize / 2 + rowOffset / 2 + y + rotOffsetY, tileSize);
-                    }
-                }
-            }
 
         }
 
@@ -158,39 +181,68 @@ class map {
                 col = -Math.floor((mouseY - y - rotOffsetY) / tileSize);
                 row = Math.floor((mouseX - x + rotOffsetX) / tileSize);
             }
-            if (col >= 0 && col < this.mapCols && row >= 0 && row < mapRows) {
+            if (col >= 0 && col < this.mapCols && row >= 0 && row < this.mapRows) {
                 this.grid[col][row].fillTile()
             }
         }
 
-        this.setTileIso = function setTileIso(mouseX, mouseY) {
-			let rotOffsetY = -this.mapRows/2*tileSize/2 -this.mapCols/2*tileSize/2
-			let rotOffsetX = this.mapRows/2*tileSize -this.mapCols/2*tileSize
-            let row = Math.floor((mouseY - y - rotOffsetY) / tileSize - (mouseX - x - rotOffsetX) / (tileSize * 2)) + 1;
+        this.findTileID = function findTileID(mouseX, mouseY) {
+			//let vOffset = 1
+            let rotOffsetY = -this.mapRows / 2 * tileSize / 2 - this.mapCols / 2 * tileSize / 2
+                let rotOffsetX = this.mapRows / 2 * tileSize - this.mapCols / 2 * tileSize
+                let row = Math.floor((mouseY - y - rotOffsetY) / tileSize - (mouseX - x - rotOffsetX) / (tileSize * 2)) + 1;
             let col = Math.floor((mouseX - x - rotOffsetX) / tileSize / 2 + (mouseY - y - rotOffsetY) / tileSize) + 1;
             if (angle == 90) {
-				rotOffsetY = this.mapRows/2*tileSize/2 -this.mapCols/2*tileSize/2 -tileSize/2
-				rotOffsetX = this.mapRows/2*tileSize +this.mapCols/2*tileSize -tileSize
-                row = Math.floor((-mouseY + y + rotOffsetY) / tileSize - (mouseX - x - rotOffsetX) / (tileSize * 2)) + 0;
+                rotOffsetY = this.mapRows / 2 * tileSize / 2 - this.mapCols / 2 * tileSize / 2 - tileSize / 2
+                    rotOffsetX = this.mapRows / 2 * tileSize + this.mapCols / 2 * tileSize - tileSize
+                    row = Math.floor((-mouseY + y + rotOffsetY) / tileSize - (mouseX - x - rotOffsetX) / (tileSize * 2)) + 0;
                 col = Math.floor((-mouseX + x + rotOffsetX) / tileSize / 2 + (mouseY - y - rotOffsetY) / tileSize) + 1;
             }
             if (angle == 180) {
-				rotOffsetY = this.mapRows/2*tileSize/2 + this.mapCols/2*tileSize/2 -tileSize
-				rotOffsetX = -this.mapRows/2*tileSize + this.mapCols/2*tileSize
-                row = Math.floor((-mouseY + y + rotOffsetY) / tileSize + (mouseX - x - rotOffsetX) / (tileSize * 2)) + 0;
+                rotOffsetY = this.mapRows / 2 * tileSize / 2 + this.mapCols / 2 * tileSize / 2 - tileSize
+                    rotOffsetX = -this.mapRows / 2 * tileSize + this.mapCols / 2 * tileSize
+                    row = Math.floor((-mouseY + y + rotOffsetY) / tileSize + (mouseX - x - rotOffsetX) / (tileSize * 2)) + 0;
                 col = Math.floor((-mouseX + x + rotOffsetX) / tileSize / 2 - (mouseY - y - rotOffsetY) / tileSize) + 0;
             }
             if (angle == 270) {
-				rotOffsetY = -this.mapRows/2*tileSize/2 +this.mapCols/2*tileSize/2 -tileSize/2
-				rotOffsetX = -this.mapRows/2*tileSize -this.mapCols/2*tileSize + tileSize
-                row = Math.floor((mouseY - y - rotOffsetY) / tileSize + (mouseX - x - rotOffsetX) / (tileSize * 2)) + 1;
+                rotOffsetY = -this.mapRows / 2 * tileSize / 2 + this.mapCols / 2 * tileSize / 2 - tileSize / 2
+                    rotOffsetX = -this.mapRows / 2 * tileSize - this.mapCols / 2 * tileSize + tileSize
+                    row = Math.floor((mouseY - y - rotOffsetY) / tileSize + (mouseX - x - rotOffsetX) / (tileSize * 2)) + 1;
                 col = Math.floor((mouseX - x - rotOffsetX) / tileSize / 2 - (mouseY - y - rotOffsetY) / tileSize) + 0;
             }
+			//console.log(map.grid[col][row]);
+            return [col, row];
+        }
+		
+		this.clearHighlights = function clearHighlights(){
+		for (let col = 0; col < this.mapCols; col++) {
+             for (let row = 0; row < this.mapRows; row++) {                             
+                  this.grid[col][row].highlight = false;
+                            }			
+		}
+		}
+		
+        this.setTileIso = function setTileIso(mouseX, mouseY) {
+            let xy = this.findTileID(mouseX, mouseY);
+            let col = xy[0];
+            let row = xy[1];
             if (col >= 0 && col < this.mapCols && row >= 0 && row < mapRows) {
                 this.grid[col][row].fillTile()
             }
         }
 
+        this.moveGroundUp = function moveGroundUp(mouseX, mouseY, altKey) {
+            let xy = this.findTileID(mouseX, mouseY);
+            let col = xy[0];
+            let row = xy[1];
+            if (col >= 0 && col < this.mapCols && row >= 0 && row < mapRows) {
+                if (altKey) {
+                    this.grid[col][row].vOffset += 0.5;
+                } else {
+                    this.grid[col][row].vOffset -= 0.5;
+                }
+            }
+        }
     }
 };
 
